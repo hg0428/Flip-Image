6d4eeb1c0c285558411092a2249f08c1f866e48b
Hudson Gouge <hudson.gouge@icloud.com>
2024-07-10 22:00:50 -0400

    update build again

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ OPTIONS = {
     "argv_emulation": True,
     "iconfile": "icons/icon.icns",
     "packages": ["PIL"],
+    "plist": {
+        "CFBundleIconFile": "icons/icon.png",  # Window icon for macOS
+        "CFBundleName": "Image Flipper",  # Application name
+        "CFBundleShortVersionString": "1.0",  # Application version
+        "CFBundleGetInfoString": "Image Flipper App",  # Information string
+        "CFBundleExecutable": "Image Flipper",  # Executable file name
+    },
 }
 
 setup(
